3e039008abe27ca51c337939882400bcac49661b
RezanAkkurt <31970405+RezanAkkurt@users.noreply.github.com>
2017-11-11 13:28:08 +0100

    Added info comment at beginning of program

--- java/OU1.java
+++ java/OU1.java
@@ -1,5 +1,9 @@
 // OU1.java
-
+/*
+Obligatorisk uppgift 1.
+Uppgiften innefattar att skapa ett program där man matar in antal veckor och antal mätningar per vecka. Sedan ska man kunna
+mata in temperaturen för varje mätning för att sedan få ut olika data för varje vecka samt hela mätperioden.
+*/
 import java.util.*; //import java.util. library. Dont have to write java.util. to use.
 
 class OU1{
