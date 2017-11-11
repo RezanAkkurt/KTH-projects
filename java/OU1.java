9ff6bc191b5fac7f0d816a013b2eef92d5e94987
RezanAkkurt <31970405+RezanAkkurt@users.noreply.github.com>
2017-11-11 14:01:10 +0100

    asd

--- java/OU1.java
+++ java/OU1.java
@@ -6,8 +6,6 @@ mata in temperaturen för varje mätning för att sedan få ut olika data för v
 */
 import java.util.*; //import java.util. library. Dont have to write java.util. to use.
 
-// test från bärbara datorn
-
 class OU1{
     public static void main(String[]args){
 
