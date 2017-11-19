58d236b1938f5e835177af2065e89f60a392e28b
Rezan Akkurt <rakkurt@kth.se>
2017-11-19 12:27:23 +0100

    fixed import

--- java/Program/OU2.java
+++ java/Program/OU2.java
@@ -1,7 +1,7 @@
 // OU2.java
 // Detta program motsvarar programmet EnTriangelOchDessCirklar i uppgiften
 
-import Library.Triangel;
+import Library.Matematik.Triangel;
 
 class OU2{
   public static void main(String[]args){
