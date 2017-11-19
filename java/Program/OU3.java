09abd6d063570a76cf4c46a43dae05760cc86198
RezanAkkurt <31970405+RezanAkkurt@users.noreply.github.com>
2017-11-19 14:21:17 +0100

    added som test lines

--- java/Program/OU3.java
+++ java/Program/OU3.java
@@ -1,5 +1,10 @@
 // OU3.java
 
 public class OU3{
+  public static void main(String[] args){
+    java.util.Scanner    in = new java.util.Scanner(System.in);
+    int k = in.nextInt();
 
+    System.out.println("Inmatat heltal = " + k);
+  }
 }
