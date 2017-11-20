06bff0c0aaa9f8de0de8e71f8f98aace8e88e45f
Rezan Akkurt <rakkurt@kth.se>
2017-11-20 13:14:48 +0100

    cleared file

--- java/Program/OU3.java
+++ java/Program/OU3.java
@@ -2,11 +2,7 @@
 
 public class OU3{
   public static void main(String[] args){
-    java.util.Scanner    in = new java.util.Scanner(System.in);
-    int k = in.nextInt();
 
-    System.out.println("Inmatat heltal = " + k);
-    System.out.println("test : " + Math.pow(k, 2));
 
   }
 }
