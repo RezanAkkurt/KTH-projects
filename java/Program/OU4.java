45a738fe8179c2b3cda459d8fcf3b823eea8d674
Rezan Akkurt <rakkurt@kth.se>
2017-11-24 13:40:57 +0100

    update OU4

--- java/Program/OU4.java
+++ java/Program/OU4.java
@@ -1,7 +1,22 @@
 // OU4.java
 
+/*******************************************************************************
+OU4 - ALGORITMER I SAMBAND MED OBJEKT
+- Räkna med teckensträngar
+
+Ett problem: utför aretmetiska operationer i samband med naturliga heltal givna
+som teckensträngar.
+
+Två naturliga heltal är givna som teckensträngar av godtycklig längd.
+
+Man ska utföra olika aritmetiska operationer i samband med dessa heltal.
+I en operation utgår man ifrån de siffror som finns i givna teckensträngar,
+och bestämmer siffrorna i resultatet, en siffra i taget.
+På så sätt skapas en ny teckensträng, som representerar resultatet av operationen.
+
+*******************************************************************************/
 public class OU4{
   public static void main(String[]args){
-    
+    // Inmatning av heltal som strings.
   }
 }
