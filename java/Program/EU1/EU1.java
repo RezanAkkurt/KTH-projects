38604c3b4b8122f7620b723f585b9d9f87b38a6c
Rezan Akkurt <rakkurt@kth.se>
2017-12-13 16:28:33 +0100

    update

--- java/Program/EU1/EU1.java
+++ java/Program/EU1/EU1.java
@@ -6,5 +6,10 @@ class EU1{
   public static void main(String[]args){
     int[] element1 = new int[16];
     int[] element2 = new int[19];
+
+    int minstaHeltalet = MinstaHeltalet.min(element1);
+
+    System.out.println(minstaHeltalet);
+
   }
 }
