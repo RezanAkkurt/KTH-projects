a50449be41a107455eb5e06a1c04c1cd5892afc1
RezanAkkurt <31970405+RezanAkkurt@users.noreply.github.com>
2017-12-21 14:56:41 +0100

    Update test file

--- java/Program/EU3/ChessboardTest.java
+++ java/Program/EU3/ChessboardTest.java
@@ -5,6 +5,8 @@ import lib.chess.NotValidFieldException;
 
 public class ChessboardTest{
   public static void main(String[]args){
+    Chessboard chessboard = new Chessboard();
 
+    System.out.println(chessboard);
   }
 }
