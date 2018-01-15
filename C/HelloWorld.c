54fe3aff38942847bd936b49a8ad3125efa49e09
RezanAkkurt <31970405+RezanAkkurt@users.noreply.github.com>
2018-01-15 22:09:15 +0100

    Added some code to test

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -1,8 +1,8 @@
 /* Hello World program */
 
-#include<stdio.h>
+#include<stdio.h> //include information about standard library
 
-int main(){
-    printf("Hello World");
+int main(){ // define a function called main that received no argument values
+    printf("Hello World \c"); //main calls library function printf to print this sequence of characters, \n represents the newline character
 
 }
