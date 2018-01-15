5b60d9f0837cea20e4e8b1d88965cec177343178
RezanAkkurt <31970405+RezanAkkurt@users.noreply.github.com>
2018-01-15 22:09:36 +0100

    update

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -3,6 +3,6 @@
 #include<stdio.h> //include information about standard library
 
 int main(){ // define a function called main that received no argument values
-    printf("Hello World \c"); //main calls library function printf to print this sequence of characters, \n represents the newline character
+    printf("Hello World \n"); //main calls library function printf to print this sequence of characters, \n represents the newline character
 
 }
