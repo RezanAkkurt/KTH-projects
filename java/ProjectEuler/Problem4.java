e66c6150bb73672aa51674c1d7054d0da31770b1
Rezan Akkurt <rakkurt@kth.se>
2018-03-06 23:37:05 +0100

    test

--- java/ProjectEuler/Problem4.java
+++ java/ProjectEuler/Problem4.java
@@ -2,6 +2,6 @@ class Problem4 {
 	public static void main(String[] args) {
 		System.out.println("Hej, detta är ett test.");
 		System.out.println("Hej, detta är ett test2.");
-		System.out.println("Hej, test från ");
+		System.out.println("Hej, test från igen");
 	}
 }
